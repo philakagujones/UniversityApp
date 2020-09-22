19b198aef652bf317b1ab27923583748543d1286
phil <philakagujones@gmail.com>
2020-09-22 11:47:09 -0700

    new app login

---
+++ scholarly/lib/screens/sign_in/components/body.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:Scholarly/components/no_account_text.dart';
+import 'package:Scholarly/components/socal_card.dart';
+import '../../../size_config.dart';
+import 'sign_form.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding:
+              EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: SizeConfig.screenHeight * 0.04),
+                Text(
+                  "Welcome Back",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: getProportionateScreenWidth(28),
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  "Sign in with your email and password",
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: SizeConfig.screenHeight * 0.08),
+                SignForm(),
+                SizedBox(height: SizeConfig.screenHeight * 0.08),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [],
+                ),
+                SizedBox(height: getProportionateScreenHeight(20)),
+                NoAccountText(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
