c64e32d5d2a1be15ad1e13a897d8795391c4d572
phil <philakagujones@gmail.com>
2020-09-24 23:16:58 -0700

    menu

--- scholarly/lib/screens/menu_dashboard/menu.dart
+++ scholarly/lib/screens/menu_dashboard/menu.dart
@@ -1,31 +1,37 @@
 import 'package:flutter/material.dart';
 
 Widget menu(context) {
-  return Padding(
-    padding: const EdgeInsets.only(left: 16.0),
-    child: Align(
-      alignment: Alignment.centerLeft,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(
-            "My Profile",
-            style: TextStyle(color: Colors.white, fontSize: 22),
+  return SlideTransition(
+    position: _slideAnimation,
+    child: ScaleTransition(
+      scale: _menuScaleAnimation,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 16.0),
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                "My Profile",
+                style: TextStyle(color: Colors.white, fontSize: 22),
+              ),
+              SizedBox(height: 10),
+              Text(
+                "Saved Applications",
+                style: TextStyle(color: Colors.white, fontSize: 22),
+              ),
+              SizedBox(height: 10),
+              Text(
+                "Settings",
+                style: TextStyle(color: Colors.white, fontSize: 22),
+              ),
+              SizedBox(height: 22),
+            ],
           ),
-          SizedBox(height: 10),
-          Text(
-            "Saved Applications",
-            style: TextStyle(color: Colors.white, fontSize: 22),
-          ),
-          SizedBox(height: 10),
-          Text(
-            "Settings",
-            style: TextStyle(color: Colors.white, fontSize: 22),
-          ),
-          SizedBox(height: 22),
-        ],
+        ),
       ),
     ),
   );
--- scholarly/lib/screens/menu_dashboard/menu_dashboard_layout.dart
+++ scholarly/lib/screens/menu_dashboard/menu_dashboard_layout.dart
@@ -1,16 +1,54 @@
 import 'package:flutter/material.dart';
 
 import './menu.dart';
+import '../home/components/body.dart';
 
-class MenuDashboard extends StatelessWidget {
+import 'package:flutter/material.dart';
+
+final Color backgroundColor = Color(0xFF4A4A58);
+
+class MenuDashboardPage extends StatefulWidget {
+  @override
+  _MenuDashboardPageState createState() => _MenuDashboardPageState();
+}
+
+class _MenuDashboardPageState extends State<MenuDashboardPage> with SingleTickerProviderStateMixin {
+  bool isCollapsed = true;
+  double screenWidth, screenHeight;
+  final Duration duration = const Duration(milliseconds: 300);
+  AnimationController _controller;
+  Animation<double> _scaleAnimation;
+  Animation<double> _menuScaleAnimation;
+  Animation<Offset> _slideAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: duration);
+    _scaleAnimation = Tween<double>(begin: 1, end: 0.8).animate(_controller);
+    _menuScaleAnimation = Tween<double>(begin: 0.5, end: 1).animate(_controller);
+    _slideAnimation = Tween<Offset>(begin: Offset(-1, 0), end: Offset(0, 0)).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    screenHeight = size.height;
+    screenWidth = size.width;
+
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: Stack(
-          children: <Widget>[
-            menu(context),
-          ],
-        ));
+      backgroundColor: backgroundColor,
+      body: Stack(
+        children: <Widget>[
+          menu(context),
+          dashboard(context),
+        ],
+      ),
+    );
   }
-}
