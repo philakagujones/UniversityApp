fae424bc5d828fff9bd8fc7903365cef8ca0c313
phil <philakagujones@gmail.com>
2020-09-27 21:43:31 -0700

    screens

--- scholarly/lib/routes.dart
+++ scholarly/lib/routes.dart
@@ -1,3 +1,5 @@
+import 'dart:js';
+
 import 'package:flutter/widgets.dart';
 import 'package:Scholarly/screens/complete_profile/complete_profile_screen.dart';
 import 'package:Scholarly/screens/forgot_password/forgot_password_screen.dart';
@@ -6,6 +8,8 @@ import 'package:Scholarly/screens/login_success/login_success_screen.dart';
 import 'package:Scholarly/screens/sign_in/sign_in_screen.dart';
 import 'package:Scholarly/screens/sign_in/sign_up/sign_up_screen.dart';
 import 'package:Scholarly/screens/splash/splash_screen.dart';
+import 'package:Scholarly/screens/saved/saved.dart';
+import 'package:Scholarly/screens/profile/profile.dart';
 
 // All our routes will be available here
 final Map<String, WidgetBuilder> routes = {
@@ -16,4 +20,6 @@ final Map<String, WidgetBuilder> routes = {
   SignUpScreen.routeName: (context) => SignUpScreen(),
   CompleteProfileScreen.routeName: (context) => CompleteProfileScreen(),
   HomeScreen.routeName: (context) => HomeScreen(),
+  SavedScreen.routeName: (context) => SavedScreen(),
+  ProfileScreen.routeName: (context) => ProfileScreen(),
 };
---
+++ scholarly/lib/screens/saved/saved.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'components/body.dart';
+
+class SavedScreen extends StatelessWidget {
+  static String routeName = "/saved";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Saved"),
+      ),
+      body: Body(),
+    );
+  }
+}
