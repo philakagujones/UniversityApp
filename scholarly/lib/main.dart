78387be6e3ae02381ddffcd8895c0867c3c74b2a
phil <philakagujones@gmail.com>
2020-09-29 19:17:30 -0700

    Update main.dart

--- scholarly/lib/main.dart
+++ scholarly/lib/main.dart
@@ -7,14 +7,14 @@ import 'package:Scholarly/theme.dart';
 
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: SplashScreen.routeName,
+      initialRoute: HomeScreen.routeName,
       home: BottomNavBar(),
       routes: routes,
       theme: theme(),
     ));
 
 // class MyApp extends StatelessWidget {
-// This widget is the root of your application.
+//  // This widget is the root of your application.
 //  @override
 // Widget build(BuildContext context) {
 // return MaterialApp(
@@ -22,7 +22,7 @@ void main() => runApp(MaterialApp(
 //  title: 'Flutter Demo',
 //  theme: theme(),
 // home: SplashScreen(),
-// We use routeName so that we dont need to remember the name
+//  // We use routeName so that we dont need to remember the name
 //  initialRoute: SplashScreen.routeName,
 //  routes: routes,
 // );
